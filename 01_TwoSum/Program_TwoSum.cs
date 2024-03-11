0cf9abd49a060267c305ef0ae1722ad0b23f846c
dadavidtseng <dadavidtseng@gmail.com>
2024-03-12 00:39:08 +0800

    init

---
+++ 01_TwoSum/Program_TwoSum.cs
@@ -0,0 +1,34 @@
+﻿namespace LeetCode._01_TwoSum;
+
+public static class Program_TwoSum {
+    public static void Main()
+    {
+        var targetArray = TwoSum([2,7,11,15], 9);
+
+        foreach (var target in targetArray)
+        { 
+            Console.WriteLine(target);
+        }
+    }
+
+    private static int[] TwoSum(int[] nums, int target) 
+    {
+        var numIndices = new Dictionary<int, int>();
+
+        for (var i = 0; i < nums.Length; i++)
+        {
+            var complement = target - nums[i];
+
+            if (numIndices.TryGetValue(complement, out var index))
+            {
+                return [index, i];
+            }
+
+            // Add the current number and its index to the dictionary
+            numIndices.TryAdd(nums[i], i);
+        }
+
+        // No solution found
+        throw new ArgumentException("No two elements add up to the target.");
+    }
+}
