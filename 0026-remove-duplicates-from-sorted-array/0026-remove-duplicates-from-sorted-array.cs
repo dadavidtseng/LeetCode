e52c33845fc172ed70c9f281ae74ee8ce4d0b1cd
Yu-Wei, Tseng <113273083+dadavidtseng@users.noreply.github.com>
2024-04-30 17:33:16 +0800

    Time: 107 ms (96.02%), Space: 50 MB (62.29%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cs
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cs
@@ -2,9 +2,6 @@ public class Solution
 {
     public int RemoveDuplicates(int[] nums) 
     {
-        if (nums.Length == 0)
-            return 0;
-
         var j = 0;
 
         for (var i = 1; i < nums.Length; i++)
