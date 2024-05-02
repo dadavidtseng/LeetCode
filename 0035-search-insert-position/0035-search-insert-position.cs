a48cfba9d9b2bbab9d5317cd10376e3bbf275541
Yu-Wei, Tseng <113273083+dadavidtseng@users.noreply.github.com>
2024-05-02 11:13:07 +0800

    Time: 67 ms (66.58%), Space: 41.6 MB (94.92%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cs
+++ 0035-search-insert-position/0035-search-insert-position.cs
@@ -2,21 +2,8 @@ public class Solution
 {
     public int SearchInsert(int[] nums, int target) 
     {
-        if (target < nums[0])
-            return 0;
-        
-        for (var i = 0; i < nums.Length; i++)
-        {
-            if (target == nums[i])
-                return i;
+        var index = Array.BinarySearch(nums, target);
 
-            if (i                  + 1 == nums.Length)
-                return nums.Length;
-            
-            if (target > nums[i] && target < nums[i + 1])
-                return i + 1;
-        }
-
-        return 0;
+        return index >= 0 ? index : ~index;
     }
 }
